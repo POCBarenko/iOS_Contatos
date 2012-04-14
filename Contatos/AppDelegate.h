c8020955d1ec0e97f01072ec11b945d5009fcef5
Rafael Caetano Pinto <barenko@gmail.com>
2012-04-14 14:58:15 -0300

    Initial Commit

---
+++ Contatos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Contatos
+//
+//  Created by ios2602 on 12/04/14.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
